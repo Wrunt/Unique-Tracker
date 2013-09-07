d2a7167a62b1e3a715f7c8e18e3309d1ed07aae2
Wruntjunior <wrunt@greyonline.net>
2013-09-07 17:40:24 -0400

    update

--- MidScreenFrame.java
+++ MidScreenFrame.java
@@ -1,40 +1,60 @@
 package poe.unique.tracker;
 
 import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 
 public class MidScreenFrame {
     public JFrame frame;
     public int fx, fy, resX, resY, offX, offY;
+    public ArrayList<String> config;
     
     public MidScreenFrame() {
-        fx = 800;
-        fy = 350;
-        
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        resX = dim.width;
-        resY = dim.height;
-        
-        offX = resX / 2 - fx / 2;
-        offY = resY / 2 - fy / 2;
-        
-        // Variable picks monitor number
-        int m = 1;
-        
-        frame = new JFrame();
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        frame.setMinimumSize(new Dimension(fx, fy)); 
-        
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice[] gd = ge.getScreenDevices();
-        GraphicsConfiguration[] gc = gd[m].getConfigurations();
-        
-        for (int i = 0; i < gc.length; i++) {
-            Rectangle gcBounds = gc[i].getBounds();
-            int xoffs = (int) gcBounds.x;
-            int yoffs = (int) gcBounds.y;
-            frame.setLocation(xoffs/2-offX, offY);
+        try {
+            Scanner sc = new Scanner(new File("data\\config.txt"));
+            config = new ArrayList<String>();
+            while (sc.hasNextLine()) {
+                    config.add(sc.nextLine());
+            }
+            
+            String mon = config.get(3);
+            
+            int m = Integer.parseInt(mon) - 1;
+            
+            fx = 800;
+            fy = 350;
+            
+            Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+            resX = dim.width;
+            resY = dim.height;
+            
+            offX = resX / 2 - fx / 2;
+            offY = resY / 2 - fy / 2;
+                   
+            frame = new JFrame();
+            frame.setResizable(false);
+            frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+            frame.setMinimumSize(new Dimension(fx, fy)); 
+            
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            GraphicsDevice[] gd = ge.getScreenDevices();
+            GraphicsConfiguration[] gc = gd[m].getConfigurations();
+            
+            for (int i = 0; i < gc.length; i++) {
+                Rectangle gcBounds = gc[i].getBounds();
+                int xoffs = (int) gcBounds.x;
+                int yoffs = (int) gcBounds.y;
+                if (xoffs < 0) {
+                    frame.setLocation(xoffs/2-offX, offY);
+                } else {
+                    frame.setLocation(xoffs/2+offX, offY);
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(MidScreenFrame.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
